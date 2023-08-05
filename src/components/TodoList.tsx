ee93c2eb14289881d2793e908b0dc63fa565535c
marrcelosantana <marcelosantana@alu.ufc.br>
2023-08-05 14:55:31 -0300

    [React-redux] Actions

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,11 +1,9 @@
-import { useSelector } from "react-redux";
+import { useAppSelector } from "../store";
 
 export function TodoList() {
-  const todos = useSelector((store) => {
+  const todos = useAppSelector((store) => {
     return store.todo;
   });
-
-  console.log(todos);
 
   return (
     <ul>
