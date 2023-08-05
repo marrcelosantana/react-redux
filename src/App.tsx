811893e3cef3f6caf639da70d59b35993ab4f7f7
marrcelosantana <marcelosantana@alu.ufc.br>
2023-08-05 14:18:24 -0300

    [React-redux] Add redux

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
-export default function App() {
+import { Provider as ReduxProvider } from "react-redux";
+
+import { AddTodo } from "./components/AddTodo";
+import { TodoList } from "./components/TodoList";
+
+import { store } from "./store";
+
+export function App() {
   return (
-    <div>
-      <h1>Hello World</h1>
-    </div>
+    <ReduxProvider store={store}>
+      <TodoList />
+      <AddTodo />
+    </ReduxProvider>
   );
 }
