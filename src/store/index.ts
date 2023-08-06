61302365dbdc8549a4fa1290fb1e925c1e3ca017
marrcelosantana <marcelosantana@alu.ufc.br>
2023-08-06 11:48:01 -0300

    [React-redux] Init new interface, add tailwind and react player

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,23 +1,9 @@
-import { configureStore, createSlice } from "@reduxjs/toolkit";
+import { configureStore } from "@reduxjs/toolkit";
 import { TypedUseSelectorHook, useSelector } from "react-redux";
 
-const todosSlice = createSlice({
-  name: "todo",
-  initialState: ["Fazer café", "Estudar Redux", "Estudar Zustand"],
-  reducers: {
-    add: (state, action) => {
-      state.push(action.payload.newTodo);
-    },
-  },
-});
-
 export const store = configureStore({
-  reducer: {
-    todo: todosSlice.reducer,
-  },
+  reducer: {},
 });
-
-export const { add } = todosSlice.actions;
 
 export type RootState = ReturnType<typeof store.getState>;
 export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
